6cf95406fcfea2d15fa102c275bf17f783220703
Paul <[omitted]>
2020-08-17 23:07:32 +0200

    Partial ES6 and above conversion.

--- TicTacToe.js
+++ TicTacToe.js
@@ -23,52 +23,39 @@ let playerController = (function () {
             return false;
         },
 
-        fillTileCollection: function (aTile) {
-            tiles[aTile] = "None";
-        },
+        fillTileCollection: (aTile) => tiles[aTile] = "None",
 
-        switchPlayer: function () {
-            activePlayer == 0 ? activePlayer = 1 : activePlayer = 0;
-        },
+        switchPlayer: () => activePlayer == 0 ? activePlayer = 1 : activePlayer = 0,
 
         checkForWinner: function () {
             console.log("Checking for a winner.")
             let tileValues = Object.values(tiles);
 
             if (tileValues[0] === users[activePlayer]) {
-                if ((tileValues[1] === users[activePlayer] && tileValues[2] === users[activePlayer]) || (tileValues[3] === users[activePlayer] && tileValues[6] === users[activePlayer])) {
+                if ((tileValues[1] === users[activePlayer] && tileValues[2] === users[activePlayer]) || (tileValues[3] === users[activePlayer] && tileValues[6] === users[activePlayer]))
                     return true;
-                }
             }
             if (tileValues[8] === users[activePlayer]) {
-                if ((tileValues[7] === users[activePlayer] && tileValues[6] === users[activePlayer]) || (tileValues[5] === users[activePlayer] && tileValues[2] === users[activePlayer])) {
+                if ((tileValues[7] === users[activePlayer] && tileValues[6] === users[activePlayer]) || (tileValues[5] === users[activePlayer] && tileValues[2] === users[activePlayer]))
                     return true;
-                }
             }
             if (tileValues[4] === users[activePlayer]) {
                 for (let upper = 0, last = 8; upper < 4; upper++, last--) {
-                    if (tileValues[upper] === users[activePlayer] && tileValues[last] === users[activePlayer]) {
+                    if (tileValues[upper] === users[activePlayer] && tileValues[last] === users[activePlayer])
                         return true;
-                    }
                 }
             }
             return false;
         },
 
-        getCurrPlayer: function () {
-            return users[activePlayer];
-        },
+        getCurrPlayer: () => users[activePlayer],
 
-        getTileStyle: function () {
-            let cssPlayerNr = activePlayer + 1;
-            return `Player${cssPlayerNr}`;
-        },
-        // Test function to add players for web building testing html:
+        getTileStyle: () => `Player${(activePlayer + 1)}`,
+        // Placeholder function to add players for testing html:
         testPlayer: function () {
             users.push("Player One");
             users.push("Player Two");
         }
-
     }
 
 })();
@@ -79,6 +66,7 @@ let UIController = (function () {
     const statustext = "Tic Tac Toe";
     const gameboardParentElement = "div#GameBoard div";
     const defaultTileClass = "Tile";
+    const resetButton = ".glass";
 
     return {
         changeSingleSquare: function (tag, cssProfile) {
@@ -87,14 +75,12 @@ let UIController = (function () {
             currTile.classList.add(cssProfile);
         },
 
-        getTilesDOM: function () {
-            return [...document.querySelectorAll(gameboardParentElement)];
-        },
+        getTilesDOM: () => [...document.querySelectorAll(gameboardParentElement)],
 
         resetAllSquares: function () {
             let allTiles = this.getTilesDOM()
             allTiles.forEach(element => {
-                element.className = 'Tile';
+                element.className = defaultTileClass;
             });
         },
 
@@ -114,73 +100,76 @@ let UIController = (function () {
 
         getStatusText: function () {
             return statustext;
+        },
+
+        getResetBtn(){
+            return resetButton;
         }
     };
 
 })();
 
 
-let controller = (function (playerC, UIC) {
-    // Main app controller: 
-    let resetBtn = ".glass";
+// Main app controller: 
+let controller = (function (playerC, uiC) {
     let setupEventListeners = function () {
-        console.log('Add listeners');
-        //  TODO : Add reset listener.
+        let resetBtn = uiC.getResetBtn();
+        let myTiles = uiC.getTilesDOM()
+        console.log('Adding event listeners');
+
         document.querySelector(resetBtn).addEventListener('click', resetGame);
-        //  Attach div id's to click event:
-        let myTiles = UIC.getTilesDOM()
+
         myTiles.forEach(element => {
-            element.addEventListener('click', handleTileSelection);
+            element.addEventListener('click', checkAndChangeTile);
             playerC.fillTileCollection(element.id);
         });
     }
-    //  Loop twice for player names:
+
     function setupPlayers() {
         for (let i = 0; i < 2; i++) {
-            playerController.addUser(UIC.playerNames(i + 1));
+            playerController.addUser(uiC.playerNames(i + 1));
         }
     }
     //  Check status of tile and change if available:
-    function handleTileSelection() {
+    function checkAndChangeTile() {
         if (playerC.verifySelection(this.id)) {
 
             document.getElementById(this.id).removeEventListener('click', this.handleTileSelection);
-            UIC.changeSingleSquare(this, playerC.getTileStyle());
-            setPlayer();
+            uiC.changeSingleSquare(this, playerC.getTileStyle());
+            setPlayerSwap();
         }
-        console.log(this.id);
-
     }
 
-    function setPlayer() {
-        if (playerC.checkForWinner() == true) {
-            UIC.updateStatusText(`${playerC.getCurrPlayer()} you won congratulations!`);
+    function setPlayerSwap() {
+        if (playerC.checkForWinner()) {
+            uiC.updateStatusText(`${playerC.getCurrPlayer()} you won congratulations!`);
         } else {
             playerC.switchPlayer();
-            UIC.updateStatusText(`${playerC.getCurrPlayer()}`);
+            uiC.updateStatusText(`${playerC.getCurrPlayer()}`);
         }
     }
 
     function resetGame() {
-        console.log('Clean start of game.')
-        UIC.resetAllSquares();
+        console.log('Reset game.')
+        uiC.resetAllSquares();
         //  Comment out 'setupPlayers' for 'playerC.testPlayer'
         //setupPlayers();
         playerC.testPlayer();
         //  Add event listeners.
         setupEventListeners();
-        UIC.updateStatusText(`${playerC.getCurrPlayer()}`)
+        uiC.updateStatusText(`${playerC.getCurrPlayer()}`)
     }
 
     return {
         init: function () {
             console.log('Init TicTacToe.js');
-            //  Comment out 'setupPlayers' for 'playerC.testPlayer'
+            //  Comment out 'setupPlayers' for 'playerC.testPlayer' also needed in 'resetGame' function.
             //setupPlayers();
             playerC.testPlayer();
-            //  Add event listeners.
+            //-------------------
+
             setupEventListeners();
-            UIC.updateStatusText(`${playerC.getCurrPlayer()}`)
+            uiC.updateStatusText(`${playerC.getCurrPlayer()}`)
         }
     };
 
